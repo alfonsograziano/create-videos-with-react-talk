5f667482ef031c3c55509d19ae5d9660c484d43b
Alfonso Graziano <alfonso.graziano@nearform.com>
2024-02-18 17:15:49 +0100

    New RomaJS custom intro

--- src/Root.tsx
+++ src/Root.tsx
@@ -3,6 +3,7 @@ import {MainSchema} from './utils/renderDescriptionSchema';
 import {Timeline} from './components/Timeline';
 import {defaultRenderDescription} from './utils/bdayVideoRenderDescription';
 import {getTotalDurationInFrames} from './utils/getTotalDurationInFrames';
+import {RomaJS, settings} from './RomaJS';
 
 export const RemotionRoot: React.FC = () => {
 	const durationInFrames = getTotalDurationInFrames(defaultRenderDescription);
@@ -13,15 +14,25 @@ export const RemotionRoot: React.FC = () => {
 	 * that will play back to back to form your video.
 	 */
 	return (
-		<Composition
-			id="MainComposition"
-			component={Timeline}
-			durationInFrames={durationInFrames}
-			fps={defaultRenderDescription.settings.format.fps}
-			width={defaultRenderDescription.settings.format.width}
-			height={defaultRenderDescription.settings.format.height}
-			schema={MainSchema}
-			defaultProps={defaultRenderDescription}
-		/>
+		<>
+			<Composition
+				id="MainComposition"
+				component={Timeline}
+				durationInFrames={durationInFrames}
+				fps={defaultRenderDescription.settings.format.fps}
+				width={defaultRenderDescription.settings.format.width}
+				height={defaultRenderDescription.settings.format.height}
+				schema={MainSchema}
+				defaultProps={defaultRenderDescription}
+			/>
+			<Composition
+				id="RomajsComposition"
+				component={RomaJS}
+				durationInFrames={settings.duration}
+				fps={settings.fps}
+				width={settings.width}
+				height={settings.height}
+			/>
+		</>
 	);
 };
